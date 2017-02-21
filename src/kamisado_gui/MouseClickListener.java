e4f0379888af20576536e4c99171fea2f4b1f785
Wojciech Cichoradzki <mjb15167@strath.ac.uk>
2017-02-21 21:34:27 +0000

    Empty commit to check whether I finally set my e-mail adress right

--- src/kamisado_gui/MouseClickListener.java
+++ src/kamisado_gui/MouseClickListener.java
@@ -54,27 +54,9 @@ public class MouseClickListener implements MouseListener{
 	
 	
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseEntered(MouseEvent e)  {}
+	public void mouseExited(MouseEvent e)   {}
+	public void mousePressed(MouseEvent e)  {}
+	public void mouseReleased(MouseEvent e) {}
 
 }
